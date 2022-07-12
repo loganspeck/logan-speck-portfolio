f9633df40918574b0e641c67193d19876a9b4224
loganspeck <loganspeck0@gmail.com>
2022-07-12 10:58:55 -0500

    Initial

---
+++ yourData.js
@@ -0,0 +1,136 @@
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "Logan Speck",
+  headerTaglineOne: "Hello 👋",
+  headerTaglineTwo: "I'm Logan Speck,",
+  headerTaglineThree: "Web Developer based",
+  headerTaglineFour: "in the United States",
+
+  socials: [
+    { title: "Github", link: "https://github.com/loganspeck" },
+    { title: "LinkedIn", link: "https://www.linkedin.com/in/logan-speck-5915b3180/" },
+    { title: "Twitter", link: "https://twitter.com/LoganAllenSpeck" },
+    { title: "Email", link: "mailto:loganspeck0@gmail/com" },
+  ],
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      title: "Auburn University School of Aviation", //Project Title - Add Your Project Title Here
+      description: "Web Development",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://www.site-shot.com/cached_image/Q4214AH4Ee2dyQJCrBEAAg",
+      //Project URL - Add Your Project Url Here
+      url: "https://cla.auburn.edu/aviation/our-programs/",
+    },
+    {
+      title: "Happy Cocktail Co", //Project Title - Add Your Project Title Here
+      description: "Web Development, Shopify Plugin Development",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://www.site-shot.com/cached_image/NMdE_gH4Ee2rQwJCrBEAAg",
+      //Project URL - Add Your Project Url Here
+      url: "https://happycocktailco.com/",
+    },
+    {
+      title: "Saturday Down South", //Project Title - Add Your Project Title Here
+      description: "Web Development",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://www.site-shot.com/cached_image/HhBA2gH4Ee2PwwJCrBEAAg",
+      //Project URL - Add Your Project Url Here
+      url: "https://www.saturdaydownsouth.com/",
+    },
+    {
+      title: "B&B Beverage Management", //Project Title - Add Your Project Title Here
+      description: "Web Development, Wordpress",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://www.site-shot.com/cached_image/Ca1E7gH4Ee2nVwJCrBEAAg",
+      //Project URL - Add Your Project Url Here
+      url: "https://beveragemgmt.com/",
+    },
+    {
+      title: "Auburn University Media Production Group", //Project Title - Add Your Project Title Here
+      description: "Web Development, Wordpress",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://www.site-shot.com/cached_image/5tQXNgH3Ee245AJCrBEABA",
+      //Project URL - Add Your Project Url Here
+      url: "https://wp.auburn.edu/mpg/",
+    },
+    {
+      title: "Soco Hernando", //Project Title - Add Your Project Title Here
+      description: "Web Development, Shopify",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://www.site-shot.com/cached_image/1KDggAH3Ee2FMgJCrBEAAg",
+      //Project URL - Add Your Project Url Here
+      url: "https://socohernando.com/",
+    },
+    {
+      title: "Michael Allen Homes", //Project Title - Add Your Project Title Here
+      description: "Web Development, SEO",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://www.site-shot.com/cached_image/faZ75gH3Ee2MXwJCrBEAAg",
+      //Project URL - Add Your Project Url Here
+      url: "https://michaelallenhomes.com/",
+    },
+
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End
+    ,{
+        title: 'Project Five',
+        description: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // Services Section ---------------
+
+  services: [
+    {
+      title: "Art Direction",
+      description:
+        "We help with the creation and development of online advertising ideas, with particular focus on their visual appearance.",
+    },
+    {
+      title: "Branding",
+      description:
+        "We design key brand elements such as the logo, color scheme, typography, and other design components that makes your brand stand out from competitors.",
+    },
+    {
+      title: "Web Design",
+      description:
+        "We build and optimize your online presence.  Website is the digital entry point into your business and a powerful revenue channel.",
+    },
+    {
+      title: "3D Design",
+      description:
+        "We combine creative design and technical skills to build striking 3D visualisations that bring your project to life.",
+    },
+  ],
+
+  // End Services Section --------------------------
+
+  // About Secton --------------
+  aboutpara:
+    "My name is Logan Speck, I am a Web Developer with over six years experience working both as a freelancer and professionally. I love to work with technologies such as HTML/CSS, JavaScript, React, Node.JS, TypeScript, Wordpress, PHP, Shopify, Static Site Generators such as Gatsby and Jekyll, and more. I am passionate about my work and will always be a lifelong learner. I am a Christian, dog dad, avid golfer, and sports enthusiast in my free time. I also love traveling, spending time with my family, and learning about how things work.",
+  //   End About Section ---------------------
+
+  // Hide Blog section
+  hideBlog: true,
+};
+
+// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
